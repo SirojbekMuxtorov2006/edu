0928dca8cccb2f58222f8cde5b0f3086bfbb7dc8
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2025-11-19 20:52:37 +0500

    home page

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+  public function homePage() {
+    return view('index');
+  }
+
+   public function aboutPage() {
+    return view('about');
+  }
+}
